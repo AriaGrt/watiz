b2b97180767921b40fb429fc76c6ef67321b4050
AriaGrt <aria.groult@outlook.fr>
2018-04-17 18:17:50 +0200

    updated first tab of navigation

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import { Navigation } from 'react-native-navigation';
 
 import Camera from './components/Camera/Camera';
 import Gallery from './components/Gallery/Gallery';
+import Result from './components/Result/Result';
 
 // register all screens of the app (including internal ones)
 export function registerScreens() {
   Navigation.registerComponent('watiz.Camera', () => Camera);
   Navigation.registerComponent('watiz.Gallery', () => Gallery);
+  Navigation.registerComponent('watiz.Result', () => Result);
 }
