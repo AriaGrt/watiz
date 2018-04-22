d8a5efb31d446034863e66c0fa6c251ec6e5ee3b
Aria Groult <agroult@agylis.com>
2018-04-22 17:00:59 +0200

    corrected capture screen style

--- components/Camera/Camera.js
+++ components/Camera/Camera.js
@@ -1,107 +1,115 @@
-'use strict';
-import React, { Component } from 'react';
+"use strict";
+import React, { Component } from "react";
 import {
-    AppRegistry,
-    Dimensions,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View,
-    PermissionsAndroid,
-    CameraRoll
-} from 'react-native';
-import { RNCamera } from 'react-native-camera';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  PermissionsAndroid,
+  CameraRoll
+} from "react-native";
+import { RNCamera } from "react-native-camera";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 //importing styles
-import { styles } from './styles';
+import { styles } from "./styles";
 
 //importing components
-import SideMenu from '../SideMenu/SideMenu';
+import SideMenu from "../SideMenu/SideMenu";
 
 export default class Camera extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            sideMenuDisplayed: false
-        }
-
-        this.requestCameraRollPermission = this.requestCameraRollPermission.bind(this)
-        this.takePicture = this.takePicture.bind(this)
-    }
-
-    componentDidMount = () => {
-        this.requestCameraRollPermission()
+    this.state = {
+      sideMenuDisplayed: false
     };
 
+    this.requestCameraRollPermission = this.requestCameraRollPermission.bind(
+      this
+    );
+    this.takePicture = this.takePicture.bind(this);
+  }
 
-    render() {
-        return (
-            <View style={styles.container} >
-                <RNCamera
-                    ref={ref => {
-                        this.camera = ref;
-                    }}
-                    style={styles.preview}
-                    type={RNCamera.Constants.Type.back}
-                    flashMode={RNCamera.Constants.FlashMode.on}
-                    permissionDialogTitle={'Permission to use camera'}
-                    permissionDialogMessage={'We need your permission to use your camera phone'}
-                >
-                    <View style={styles.header}>
-                        <View style={styles.topWrapper}>
-                            <TouchableOpacity style={styles.touchableOpacityMenuIcon} onPress={this.displayMenu}>
-                                <Icon name="menu" style={styles.menuIcon} />
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                    <View style={styles.snapWrapper}>
-                        <TouchableOpacity style={styles.snap} onPress={this.takePicture} />
-                    </View>
-                    <View style={styles.galleryWrapper}>
-                        <TouchableOpacity style={styles.toGalleryTouchable} onPress={console.log('going to library')}>
-                            <Icon name="collections" color="#4F8EF7" style={styles.toGallery} />
-                        </TouchableOpacity>
-                    </View>
-                </RNCamera>
-                {this.manipulateSideMenu()}
+  componentDidMount = () => {
+    this.requestCameraRollPermission();
+  };
 
+  takePicture = async function() {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      CameraRoll.saveToCameraRoll(data.uri);
+    }
+  };
+
+  requestCameraRollPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE
+      );
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  manipulateSideMenu = () => {
+    if (this.state.sideMenuDisplayed) {
+      return <SideMenu />;
+    }
+  };
+
+  displayMenu = () => {
+    this.setState({
+      sideMenuDisplayed: true
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          permissionDialogTitle={"Permission to use camera"}
+          permissionDialogMessage={
+            "We need your permission to use your camera phone"
+          }
+        >
+          <View style={styles.header}>
+            <View style={styles.topWrapper}>
+              <TouchableOpacity
+                style={styles.touchableOpacityMenuIcon}
+                onPress={this.displayMenu}
+              >
+                <Icon name="menu" style={styles.menuIcon} />
+              </TouchableOpacity>
             </View>
-        );
-    }
-
-    takePicture = async function () {
-        if (this.camera) {
-            const options = { quality: 0.5, base64: true };
-            const data = await this.camera.takePictureAsync(options)
-            CameraRoll.saveToCameraRoll(data.uri)
-        }
-    };
-
-    requestCameraRollPermission = async () => {
-        try {
-            const granted = await PermissionsAndroid.request(
-                PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE
-            )
-        }
-        catch (err) {
-            console.warn(err)
-        }
-    }
-
-    manipulateSideMenu = () => {
-        if (this.state.sideMenuDisplayed) {
-            return <SideMenu />
-        }
-    };
-
-    displayMenu = () => {
-        this.setState({
-            sideMenuDisplayed: true
-        })
-    };
-
-
+          </View>
+          <View style={styles.footer}>
+            <TouchableOpacity
+              style={styles.iconWrapper}
+              onPress={console.log("going to library")}
+            >
+              <Icon name="collections" style={styles.icon} />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.snap} onPress={this.takePicture} />
+            <TouchableOpacity
+              style={styles.iconWrapper}
+              onPress={console.log("going to library")}
+            >
+              <Icon name="collections" style={styles.icon} />
+            </TouchableOpacity>
+          </View>
+        </RNCamera>
+        {this.manipulateSideMenu()}
+      </View>
+    );
+  }
 }
--- components/Camera/styles.js
+++ components/Camera/styles.js
@@ -1,73 +1,77 @@
-import { StyleSheet } from 'react-native';
-import { responsiveHeight, responsiveFontSize, responsiveWidth } from 'react-native-responsive-dimensions';
+import { StyleSheet } from "react-native";
+import {
+  responsiveHeight,
+  responsiveFontSize,
+  responsiveWidth
+} from "react-native-responsive-dimensions";
 
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-    },
-    // Camera Styling
-    preview: {
-        flex: 1,
-    },
-    //Menu Button Styling
-    header: {
-        position: 'absolute',
-        width: '100%',
-        top: 20,
-        zIndex: 0
-    },
-    topWrapper: {
-        flex: 0,
-        flexDirection: 'row',
-        alignItems: 'center',
-        height: responsiveHeight(9),
-    },
-    touchableOpacityMenuIcon: {
-        marginLeft: 30,
-        height: responsiveWidth(10),
-        width: responsiveWidth(10),
-        borderRadius: responsiveWidth(10),
-        backgroundColor: '#1DE9B6',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    menuIcon: {
-        fontSize: responsiveFontSize(4),
-        color: 'white'
-    },
-    snapWrapper: {
-        position: 'absolute',
-        height: responsiveHeight(9),
-        bottom: 30,
-        flex: 0,
-        width: '100%',
-        flexDirection: 'row',
-        justifyContent: 'center'
-    },
-    snap: {
-        borderWidth: 4,
-        borderColor: 'white',
-        height: responsiveWidth(15),
-        width: responsiveWidth(15),
-        borderRadius: responsiveWidth(15),
-        backgroundColor: 'transparent',
-    },
-    galleryWrapper: {
-        position: 'absolute',
-        height: responsiveHeight(9),
-        bottom: 30,
-        flex: 0,
-        width: '100%',
-        flexDirection: 'row',
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-    },
-    toGalleryTouchable: {
-        marginRight: responsiveWidth(10)
-    },
-    toGallery: {
-        color: 'white',
-        fontSize: responsiveFontSize(5)
-    }
+  container: {
+    flex: 1,
+    flexDirection: "column"
+  },
+  // Camera Styling
+  preview: {
+    flex: 1
+  },
+  //Menu Button Styling
+  header: {
+    position: "absolute",
+    width: "100%",
+    top: 20,
+    zIndex: 0
+  },
+  topWrapper: {
+    flex: 0,
+    flexDirection: "row",
+    alignItems: "center",
+    height: responsiveHeight(9)
+  },
+  touchableOpacityMenuIcon: {
+    marginLeft: 30,
+    height: responsiveWidth(10),
+    width: responsiveWidth(10),
+    borderRadius: responsiveWidth(10),
+    backgroundColor: "#1DE9B6",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  menuIcon: {
+    fontSize: responsiveFontSize(4),
+    color: "white"
+  },
+  footer: {
+    position: "absolute",
+    bottom: 20,
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: responsiveHeight(15)
+  },
+  iconWrapper: {
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#1DE9B6",
+    width: responsiveWidth(12),
+    height: responsiveWidth(12),
+    borderRadius: responsiveWidth(12),
+    marginLeft: 40,
+    marginRight: 40
+  },
+  icon: {
+    fontSize: responsiveFontSize(4),
+    color: "#EFEFEF"
+  },
+  snapWrapper: {},
+  snap: {
+    width: responsiveWidth(17),
+    height: responsiveWidth(17),
+    backgroundColor: "transparent",
+    borderRadius: responsiveWidth(17),
+    borderWidth: 4,
+    borderColor: "#EFEFEF"
+  }
 });
